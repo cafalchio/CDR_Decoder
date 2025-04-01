ffe785ea0bbcf3d6b2382d8722187ae37c876f0e
cafalchio <maolivei@tcd.ie>
2025-04-01 22:34:18 +0100

    feat: implement display for types and TimeStamp type

--- src/datatypes/primitives.rs
+++ src/datatypes/primitives.rs
@@ -1,54 +1,103 @@
+use std::fmt::{self};
 
-#[allow(dead_code)]
-
-pub struct Hex {
-    value: u8,
-}
-
-pub struct Hex2 {
-    value: [u8; 2],
-}
-
-pub struct HexWord {
-    value: [u8; 4],
-}
-
-pub struct HexDuword {
-    value: [u8; 4],
-}
-
+#[derive(Debug, Clone, Copy)]
 pub struct BCD {
     value: u8,
 }
 
 impl BCD {
     pub fn as_dec(&self) -> u8 {
-        let low = self.value & 0b00001111;
-        let high = (self.value & 0b11110000) >> 4;
-        low + (high * 10)
+        let low = self.value & 0x0F;
+        let high = (self.value >> 4) & 0x0F;
+        high * 10 + low
     }
 }
-pub struct BCDWord {
-    value: [BCD; 2],
-}
-impl BCDWord {
-    pub fn as_dec(&self) -> u16 {
-        let high = self.value[0].as_dec() as u16;
-        let low = self.value[1].as_dec() as u16;
-        high * 100 + low
+impl fmt::Display for BCD {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:02X}",
+            self.as_dec()
+        )
     }
 }
 
-pub struct BCDDuword {
-    value: [BCDWord; 2],
+#[derive(Debug, Clone, Copy)]
+pub struct BCDWord {
+    high: BCD,
+    low: BCD,
 }
-impl BCDDuword {
-    pub fn as_dec(&self) -> u32 {
-        let high = self.value[0].as_dec() as u32;
-        let low = self.value[1].as_dec() as u32;
-        high * 10000 + low
+
+impl BCDWord {
+    pub fn as_dec(&self) -> u16 {
+        self.high.as_dec() as u16 * 100 + self.low.as_dec() as u16
+    }
+
+}
+impl fmt::Display for BCDWord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:02X}{:02X})",
+            self.high.as_dec(), self.low.as_dec()
+        )
     }
 }
-pub struct Timestamp {
-    value: [u8; 5],
+
+#[derive(Debug, Clone, Copy)]
+pub struct BcdTimestamp {
+    sec: BCD,
+    min: BCD,
+    hour: BCD,
+    day: BCD,
+    month: BCD,
+    year: BCDWord,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct BCD2uword {
+    low : BCDWord,
+    high: BCDWord
+}
+impl fmt::Display for BCD2uword {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:02X}{:02X}",
+            self.high.as_dec(), self.low.as_dec()
+        )
+    }
+}
+
+impl BcdTimestamp {
+    pub fn from_hex_dump(hex_dump: [u8; 7]) -> Self {
+        Self {
+            sec: BCD { value: hex_dump[0] },
+            min: BCD { value: hex_dump[1] },
+            hour: BCD { value: hex_dump[2] },
+            day: BCD { value: hex_dump[3] },
+            month: BCD { value: hex_dump[4] },
+            year: BCDWord {
+                low: BCD { value: hex_dump[5] },
+                high: BCD { value: hex_dump[6] },
+            },
+        }
+    }
+
+    pub fn to_bcd_string(&self) -> String {
+        format!(
+            "{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}H",
+            self.year.high.value, self.year.low.value, self.month.value, self.day.value, self.hour.value, self.min.value, self.sec.value
+        )
+    }
+}
+
+impl fmt::Display for BcdTimestamp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Time: {:02}:{:02}:{:02} Date: {:02}/{:02}/{}",
+            self.hour.as_dec(), self.min.as_dec(), self.sec.as_dec(), self.day.as_dec(), self.month.as_dec(), self.year.as_dec()
+        )
+    }
 }
---
+++ src/datatypes/tests.rs
@@ -0,0 +1,23 @@
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bcd_word_as_dec() {
+        let high = BCD { value: 0x12 };  
+        let low = BCD { value: 0x34 };   
+        let bcd_word = BCDWord { high, low };
+        assert_eq!(bcd_word.as_dec(), 1234);
+    }
+
+    #[test]
+    fn test_bcd_word_display() {
+        // Create BCD for high (12) and low (34)
+        let high = BCD { value: 0x12 };  
+        let low = BCD { value: 0x34 }; 
+        let bcd_word = BCDWord { high, low };
+        let expected_display = "1234";
+        assert_eq!(format!("{}", bcd_word), expected_display);
+    }
+}
